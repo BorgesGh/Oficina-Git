8b998edfbef338b8e04dbd2f42bd4c1df44c5762
Ghabriel Borges <ghabriel@imdigital.com>
2025-09-26 12:20:17 -0300

    Adicionar ambos os projetos em 1 branch

---
+++ src/backend/main.py
@@ -0,0 +1,8 @@
+# Crie um programa que: 
+# Solicite ao usuário um número inteiro
+# Retorne quantos números primos existem entre 1 e o número digitado e quais são eles.
+
+# Exemplo:
+
+# Entrada: 25
+# Saída: 9 (números primos entre 1 e 25: 2, 3, 5, 7, 11, 13, 17, 19, 23)
